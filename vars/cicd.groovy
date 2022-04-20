6d45a1997aaaf99875383c675d5b1017b7312d24
SAGAR SONAWANE <103016750+SAGARSGS@users.noreply.github.com>
2022-04-20 11:36:16 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,10 +6,9 @@ def newMaven()
 {
     sh 'mvn package'
 }
-def newDeploy(app,j,ip,f)
+def newDeploy(j,ip,f)
 {
-     echo "${app}"
-     sh "scp /home/ubuntu/.jenkins/workspace/${j}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${f}.war"
+     "sh scp /home/ubuntu/.jenkins/workspace/${j}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${f}.war"
 }
 def newTest(appname)
 {
